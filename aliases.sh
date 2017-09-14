b823739876bccea3059dfd954c41c9e2c34d9a49
Andrew Wood <andrew.wood@experiencepoint.com>
2017-09-13 22:25:52 -0400

    aliases

--- aliases.sh
+++ aliases.sh
@@ -25,7 +25,7 @@ function getBranchName() {
     branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
     branch_name="(unnamed branch)"     # detached HEAD
     branch_name=${branch_name##refs/heads/}
-    git pull origin $branch_name
+    git pull origin "$branch_name"
 }
 
 alias sql=connectToSql
